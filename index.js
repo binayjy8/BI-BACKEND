c1c44297b16ba76e3954924d8b1854bcc6c39f94
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-22 16:21:08 +0530

    set one api

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ app.use(express.json());
 
 initializeDatabase();
 
+async function findAllMeetUp() {
+    try{
+        const data = await Event.find();
+        return data;
+    }catch(error){
+        throw error;
+    }
+}
+
+app.get("/all", async(req, res) => {
+    try{
+        const event = await findAllMeetUp();
+        if(event){
+            res.status(200).json({message: "Events fetched successfully", event: event});
+        }else{
+            res.status(404).json({error: "Couldn't find any data"});
+        }
+    }catch(error){
+        res.status(500).json({message: "Internal server problem"});
+    }
+});
+
 app.listen(PORT, () => {
     console.log("Listening to the port", PORT);
 });
