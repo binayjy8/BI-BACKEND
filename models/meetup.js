374ecd1ffdc653f4038bbd2d6316e4145e8541cf
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-22 15:41:15 +0530

    mongoose model

--- models/meetup.js
+++ models/meetup.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+
+const meetUp = mongoose.Schema({
+    title: {
+        type: String,
+    },
+    eventType: {
+        type: String,
+        enum: ["Online Event", "Offline Event"],
+    },
+    dateTime: {
+        type: Date,
+    },
+    category: {
+        type: String,
+    },
+    imageUrl: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+});
